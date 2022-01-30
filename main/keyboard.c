a76d4f37414ca8cb6d1a46eda2de363e3375d416
light <hcchen270@gmail.com>
2022-01-30 10:25:32 +0800

    Bluetooth functions complete, very slow!

--- main/keyboard.c
+++ main/keyboard.c
@@ -1,18 +1,91 @@
 #include <stdio.h>
+#include<string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/event_groups.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "esp_system.h"
+#include "esp_wifi.h"               
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_bt.h"
+
+#include "esp_hidd_prf_api.h"
+#include "esp_bt_defs.h"
+#include "esp_gap_ble_api.h"
+#include "esp_gatts_api.h"
+#include "esp_gatt_defs.h"
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+#include "driver/gpio.h"
+#include "hid_dev.h"
 
 /**
  * @brief This version is tested on a 4*5 numpad matrix.
  * 
  */
 
-const uint16_t cPins[4] = {16, 17, 5, 18};
-const uint16_t rPins[5] = {23, 19, 22, 4, 0};
+struct key_pair
+{
+    int key;
+    char action;
+};
 
+static void hidd_event_callback(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param);
+#define HIDD_DEVICE_NAME "HID_KEYBOARD"
+#define HID_DEMO_TAG "HID_DEMO"
+static uint16_t hid_conn_id = 0;
+static bool sec_conn = false;
+#define CHAR_DECLARATION_SIZE   (sizeof(uint8_t))
+static uint8_t hidd_service_uuid128[] = {
+    /* LSB <--------------------------------------------------------------------------------> MSB */
+    //first uuid, 16bit, [12],[13] is the value
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x12, 0x18, 0x00, 0x00,
+};
+
+static esp_ble_adv_data_t hidd_adv_data = {
+    .set_scan_rsp = false,
+    .include_name = true,
+    .include_txpower = true,
+    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+    .appearance = 0x03c0,       //HID Generic,
+    .manufacturer_len = 0,
+    .p_manufacturer_data =  NULL,
+    .service_data_len = 0,
+    .p_service_data = NULL,
+    .service_uuid_len = sizeof(hidd_service_uuid128),
+    .p_service_uuid = hidd_service_uuid128,
+    .flag = 0x6,
+};
+
+static esp_ble_adv_params_t hidd_adv_params = {
+    .adv_int_min        = 0x20,
+    .adv_int_max        = 0x30,
+    .adv_type           = ADV_TYPE_IND,
+    .own_addr_type      = BLE_ADDR_TYPE_PUBLIC,
+    //.peer_addr            =
+    //.peer_addr_type       =
+    .channel_map        = ADV_CHNL_ALL,
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+};
+
+const int cPins[4] = {16, 17, 5, 18};
+const int rPins[5] = {23, 19, 22, 4, 0};
+const uint8_t outputs[20] = {
+    HID_KEY_NUM_LOCK, HID_KEY_DIVIDE, HID_KEY_MULTIPLY, HID_KEY_DELETE,
+    HID_KEYPAD_7, HID_KEYPAD_8, HID_KEYPAD_9, HID_KEY_SUBTRACT,
+    HID_KEYPAD_4, HID_KEYPAD_5, HID_KEYPAD_6, HID_KEY_ADD,
+    HID_KEYPAD_1, HID_KEYPAD_2, HID_KEYPAD_3, HID_KEY_EQUAL,
+    HID_KEY_TAB, HID_KEYPAD_0, HID_KEYPAD_DOT, HID_KEY_ENTER 
+};
+
+// static QueueHandle_t xqueue = NULL;
 bool pressed[20], previous_pressed[20];
+struct key_pair this_key, that_key;
 
 void matrix_setup() {
     for(int i = 0; i < 20; i++) {
@@ -44,19 +117,141 @@ void matrix_scan() {
                 else {
                     pressed[i * 4 + j] = false;
                 }
-                vTaskDelay(10 / portTICK_PERIOD_MS);
+                vTaskDelay(2);
             }
             gpio_set_level(rPins[i], 0);
         }
 
-        for(int i = 0; i < 20; i++) {
-            if(pressed[i] == true && previous_pressed[i] == false) {
-                printf("%i pressed \n", i);
+    //     for(int i = 0; i < 20; i++) {
+    //         if(pressed[i] == true && previous_pressed[i] == false) {
+    //             this_key.key = i;
+    //             this_key.action = 'p';
+    //             xQueueSend(xqueue, &this_key, 0U);
+    //         }
+    //         else if(pressed[i] == false && previous_pressed[i] == true) {
+    //             this_key.key = i;
+    //             this_key.action = 'r';
+    //             xQueueSend(xqueue, &this_key, 0U);
+    //         }
+    //         previous_pressed[i] = pressed[i];
+    //     }
+    }
+}
+
+// void print_value(void *pvParameter) {
+//     while(true) {
+//         if(xqueue != 0) {
+//             xQueueReceive(xqueue, &that_key, portMAX_DELAY);
+//             printf("%i %c \n", that_key.key, that_key.action);
+//         }
+//         else {
+//             printf("nothing\n");
+//             vTaskDelay(1);
+//         }
+//     }
+// }
+
+static void hidd_event_callback(esp_hidd_cb_event_t event, esp_hidd_cb_param_t *param)
+{
+    switch(event) {
+        case ESP_HIDD_EVENT_REG_FINISH: {
+            if (param->init_finish.state == ESP_HIDD_INIT_OK) {
+                //esp_bd_addr_t rand_addr = {0x04,0x11,0x11,0x11,0x11,0x05};
+                esp_ble_gap_set_device_name(HIDD_DEVICE_NAME);
+                esp_ble_gap_config_adv_data(&hidd_adv_data);
+
             }
-            else if(pressed[i] == false && previous_pressed[i] == true) {
-                printf("%i released \n", i);
+            break;
+        }
+        case ESP_BAT_EVENT_REG: {
+            break;
+        }
+        case ESP_HIDD_EVENT_DEINIT_FINISH:
+	        break;
+		case ESP_HIDD_EVENT_BLE_CONNECT: {
+            ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_CONNECT");
+            hid_conn_id = param->connect.conn_id;
+            break;
+        }
+        case ESP_HIDD_EVENT_BLE_DISCONNECT: {
+            sec_conn = false;
+            ESP_LOGI(HID_DEMO_TAG, "ESP_HIDD_EVENT_BLE_DISCONNECT");
+            esp_ble_gap_start_advertising(&hidd_adv_params);
+            break;
+        }
+        case ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT: {
+            ESP_LOGI(HID_DEMO_TAG, "%s, ESP_HIDD_EVENT_BLE_VENDOR_REPORT_WRITE_EVT", __func__);
+            ESP_LOG_BUFFER_HEX(HID_DEMO_TAG, param->vendor_write.data, param->vendor_write.length);
+        }
+        default:
+            break;
+    }
+    return;
+}
+
+static void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param)
+{
+    switch (event) {
+    case ESP_GAP_BLE_ADV_DATA_SET_COMPLETE_EVT:
+        esp_ble_gap_start_advertising(&hidd_adv_params);
+        break;
+     case ESP_GAP_BLE_SEC_REQ_EVT:
+        for(int i = 0; i < ESP_BD_ADDR_LEN; i++) {
+             ESP_LOGD(HID_DEMO_TAG, "%x:",param->ble_security.ble_req.bd_addr[i]);
+        }
+        esp_ble_gap_security_rsp(param->ble_security.ble_req.bd_addr, true);
+	 break;
+     case ESP_GAP_BLE_AUTH_CMPL_EVT:
+        sec_conn = true;
+        esp_bd_addr_t bd_addr;
+        memcpy(bd_addr, param->ble_security.auth_cmpl.bd_addr, sizeof(esp_bd_addr_t));
+        ESP_LOGI(HID_DEMO_TAG, "remote BD_ADDR: %08x%04x",\
+                (bd_addr[0] << 24) + (bd_addr[1] << 16) + (bd_addr[2] << 8) + bd_addr[3],
+                (bd_addr[4] << 8) + bd_addr[5]);
+        ESP_LOGI(HID_DEMO_TAG, "address type = %d", param->ble_security.auth_cmpl.addr_type);
+        ESP_LOGI(HID_DEMO_TAG, "pair status = %s",param->ble_security.auth_cmpl.success ? "success" : "fail");
+        if(!param->ble_security.auth_cmpl.success) {
+            ESP_LOGE(HID_DEMO_TAG, "fail reason = 0x%x",param->ble_security.auth_cmpl.fail_reason);
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+void send_keys(void *pvParameters) {
+    int num_pressed;
+    uint8_t *pointer;
+    while(true) {
+        if(sec_conn) {
+            num_pressed = 0;
+            for(int i = 0; i < 20; i++) {
+                if(pressed[i]) num_pressed++;
             }
-            previous_pressed[i] = pressed[i];
+
+            if(num_pressed) {
+                pointer = malloc(num_pressed * sizeof(uint8_t));
+                int j = 0;
+                for(int i = 0; i < 20; i++) {
+                    if(pressed[i]) {
+                        // printf("%i \n", i);
+                        *(pointer + j * 4) = outputs[i];
+                        j++;
+                    }
+                }
+                // for(int i = 0; i < num_pressed; i++) {
+                //     printf("%u ", *(pointer + i));
+                // }
+                // printf("\n");
+                esp_hidd_send_keyboard_value(hid_conn_id, 0, pointer, num_pressed);
+                free(pointer);
+            }
+            else {
+                pointer = calloc(1, sizeof(uint8_t));
+                esp_hidd_send_keyboard_value(hid_conn_id, 0, pointer, 0);
+                free(pointer);
+            }
+            vTaskDelay(15);
         }
     }
 }
@@ -64,7 +259,71 @@ void matrix_scan() {
 void app_main(void) {
     matrix_setup();
 
-    xTaskCreate(matrix_scan, "scan", 8192, NULL, 1, NULL);
+    esp_err_t ret;
 
-    
+    // Initialize NVS.
+    ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
+
+    esp_bt_controller_config_t bt_cfg = BT_CONTROLLER_INIT_CONFIG_DEFAULT();
+    ret = esp_bt_controller_init(&bt_cfg);
+    if (ret) {
+        ESP_LOGE(HID_DEMO_TAG, "%s initialize controller failed\n", __func__);
+        return;
+    }
+
+    ret = esp_bt_controller_enable(ESP_BT_MODE_BLE);
+    if (ret) {
+        ESP_LOGE(HID_DEMO_TAG, "%s enable controller failed\n", __func__);
+        return;
+    }
+
+    ret = esp_bluedroid_init();
+    if (ret) {
+        ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed\n", __func__);
+        return;
+    }
+
+    ret = esp_bluedroid_enable();
+    if (ret) {
+        ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed\n", __func__);
+        return;
+    }
+
+    if((ret = esp_hidd_profile_init()) != ESP_OK) {
+        ESP_LOGE(HID_DEMO_TAG, "%s init bluedroid failed\n", __func__);
+    }
+
+    ///register the callback function to the gap module
+    esp_ble_gap_register_callback(gap_event_handler);
+    esp_hidd_register_callbacks(hidd_event_callback);
+
+    /* set the security iocap & auth_req & key size & init key response key parameters to the stack*/
+    esp_ble_auth_req_t auth_req = ESP_LE_AUTH_BOND;     //bonding with peer device after authentication
+    esp_ble_io_cap_t iocap = ESP_IO_CAP_NONE;           //set the IO capability to No output No input
+    uint8_t key_size = 16;      //the key size should be 7~16 bytes
+    uint8_t init_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+    uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+    esp_ble_gap_set_security_param(ESP_BLE_SM_AUTHEN_REQ_MODE, &auth_req, sizeof(uint8_t));
+    esp_ble_gap_set_security_param(ESP_BLE_SM_IOCAP_MODE, &iocap, sizeof(uint8_t));
+    esp_ble_gap_set_security_param(ESP_BLE_SM_MAX_KEY_SIZE, &key_size, sizeof(uint8_t));
+    /* If your BLE device act as a Slave, the init_key means you hope which types of key of the master should distribute to you,
+    and the response key means which key you can distribute to the Master;
+    If your BLE device act as a master, the response key means you hope which types of key of the slave should distribute to you,
+    and the init key means which key you can distribute to the slave. */
+    esp_ble_gap_set_security_param(ESP_BLE_SM_SET_INIT_KEY, &init_key, sizeof(uint8_t));
+    esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
+
+    // xqueue = xQueueCreate(16, sizeof(int) + 2 * sizeof(char));
+
+    // while(!sec_conn) {};
+    xTaskCreatePinnedToCore(matrix_scan, "scan", 8192, NULL, 1, NULL, 1);
+    // xTaskCreatePinnedToCore(print_value, "print", 8192, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(send_keys, "send", 8192, NULL, 1, NULL, 0);
 }
